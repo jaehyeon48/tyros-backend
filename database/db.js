e4c358b6d29441b007f4458890be3172bb6be4c0
jaehyeon48 <imjaehyeon48@gmail.com>
2020-08-05 13:41:17 +0900

    Changed connection logic to DB

--- database/db.js
+++ database/db.js
@@ -1,36 +1,42 @@
-const mysql = require('mysql');
+// let connection;
+
+// async function handleConnectDB() {
+//   connection = await mysql.createConnection(db_config);
+
+//   connection.connect((err) => {
+//     if (err) {
+//       console.log('error when connecting to db:', err);
+//       setTimeout(handleConnectDB, 2000);
+//     }
+//     console.log('Mysql Database Connected...');
+//   });
+//   connection.on('error', (err) => {
+//     console.log('db error', err);
+//     if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+//       handleConnectDB();
+//     }
+//     else {
+//       throw err;
+//     }
+//   });
+// }
+
+// handleConnectDB();
+
+// module.exports = connection;
+
+const mysql = require('mysql2');
 require('dotenv').config();
 
 const db_config = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  waitForConnections: true
 };
 
-let connection;
 
-function handleConnectDB() {
-  connection = mysql.createConnection(db_config);
+const pool = mysql.createPool(db_config);
 
-  connection.connect((err) => {
-    if (err) {
-      console.log('error when connecting to db:', err);
-      setTimeout(handleConnectDB, 2000);
-    }
-    console.log('Mysql Database Connected...');
-  });
-  connection.on('error', (err) => {
-    console.log('db error', err);
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-      handleConnectDB();
-    }
-    else {
-      throw err;
-    }
-  });
-}
-
-handleConnectDB();
-
-module.exports = connection;
+module.exports = pool.promise();
