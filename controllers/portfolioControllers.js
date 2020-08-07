0f2da25521f96f1fca38e3f06c0ee7c278db46b6
jaehyeon48 <imjaehyeon48@gmail.com>
2020-08-07 18:00:47 +0900

    Added modify portfolio name route and its controller

--- controllers/portfolioControllers.js
+++ controllers/portfolioControllers.js
@@ -17,6 +17,58 @@ async function getPortfolios(req, res) {
 }
 
 
+// @ROUTE         GET api/portfolio/:portfolioId/stocks
+// @DESCRIPTION   Get Portfolio's stocks
+// @ACCESS        Private
+async function getPortfolioStocks(req, res) {
+  const userId = req.user.id;
+  const portfolioId = req.params.portfolioId;
+  const getStocksQuery = `
+    SELECT stocks.ticker, stocks.company_name, stocks.price, stocks.quantity, 
+    stocks.transaction_type, stocks.transaction_date
+    FROM users
+      INNER JOIN portfolios
+        ON users.user_id = ${userId} AND portfolios.portfolio_id = ${portfolioId} AND users.user_id = portfolios.owner_id 
+      INNER JOIN stocks
+        ON users.user_id = stocks.holder_id AND portfolios.portfolio_id = stocks.portfolio_id
+      ORDER BY stocks.ticker, stocks.transaction_date, stocks.transaction_type;`;
+
+  try {
+    const [stocksRow] = await pool.query(getStocksQuery);
+    res.status(200).json(stocksRow);
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ errorMsg: 'Internal Server Error' });
+  }
+}
+
+
+// @ROUTE         GET api/portfolio/:portfolioId/cash
+// @DESCRIPTION   Get Portfolio's cash
+// @ACCESS        Private
+async function getPortfolioCash(req, res) {
+  const userId = req.user.id;
+  const portfolioId = req.params.portfolioId;
+  const getCashQuery = `
+    SELECT cash.amount, cash.transaction_type, cash.transaction_date
+    FROM users
+	    INNER JOIN portfolios
+		    ON users.user_id = ${userId} AND portfolios.portfolio_id = ${portfolioId} AND users.user_id = portfolios.owner_id
+	    INNER JOIN cash
+		    ON users.user_id = cash.holder_id AND portfolios.portfolio_id = cash.portfolio_id
+	    ORDER BY transaction_date;
+  `;
+
+  try {
+    const [cashRow] = await pool.query(getCashQuery);
+    res.status(200).json(cashRow);
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ errorMsg: 'Internal Server Error' });
+  }
+}
+
+
 // @ROUTE         POST api/portfolio
 // @DESCRIPTION   Create New Portfolio
 // @ACCESS        Private
@@ -34,7 +86,26 @@ async function createPortfolio(req, res) {
 }
 
 
+// @ROUTE         PUT api/portfolio/:portfolioId
+// @DESCRIPTION   Edit Portfolio's Name
+// @ACCESS        Private
+async function editPortfolioName(req, res) {
+  const { newPortfolioName } = req.body;
+  const portfolioId = req.params.portfolioId;
+  try {
+    await pool.query(`UPDATE portfolios SET portfolio_name = '${newPortfolioName}' WHERE portfolio_id = ${portfolioId}`);
+
+    res.status(200).json({ successMsg: 'Successfully changed portfolio name.' });
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ errorMsg: 'Internal Server Error' });
+  }
+}
+
 module.exports = {
   getPortfolios,
-  createPortfolio
+  getPortfolioStocks,
+  getPortfolioCash,
+  createPortfolio,
+  editPortfolioName
 };
--- routes/portfolioRoute.js
+++ routes/portfolioRoute.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 const {
   getPortfolios,
-  createPortfolio
+  getPortfolioStocks,
+  getPortfolioCash,
+  createPortfolio,
+  editPortfolioName
 } = require('../controllers/portfolioControllers');
 
 const authMiddleware = require('../middlewares/authMiddleware');
@@ -15,9 +18,26 @@ const authMiddleware = require('../middlewares/authMiddleware');
 router.get('/', authMiddleware, getPortfolios);
 
 
+// @ROUTE         GET api/portfolio/:portfolioId/stocks
+// @DESCRIPTION   Get Portfolio's stocks
+// @ACCESS        Private
+router.get('/:portfolioId/stocks', authMiddleware, getPortfolioStocks);
+
+
+// @ROUTE         GET api/portfolio/:portfolioId/cash
+// @DESCRIPTION   Get Portfolio's cash
+// @ACCESS        Private
+router.get('/:portfolioId/cash', authMiddleware, getPortfolioCash);
+
 // @ROUTE         POST api/portfolio
 // @DESCRIPTION   Create New Portfolio
 // @ACCESS        Private
 router.post('/', authMiddleware, createPortfolio);
+
+
+// @ROUTE         PUT api/portfolio/:portfolioId
+// @DESCRIPTION   Edit Portfolio's Name
+// @ACCESS        Private
+router.put('/:portfolioId', authMiddleware, editPortfolioName);
 
 module.exports = router;
