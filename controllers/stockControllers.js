9c4393305dd078773125aa84b806b0dd728d4113
jaehyeon48 <imjaehyeon48@gmail.com>
2020-08-08 17:15:29 +0900

    Added edit and delete stock route and their controllers

--- controllers/stockControllers.js
+++ controllers/stockControllers.js
@@ -23,6 +23,49 @@ async function addStock(req, res) {
   }
 }
 
+
+// @ROUTE         PUT api/stock/:stockId
+// @DESCRIPTION   Edit Stock's Information
+// @ACCESS        Private
+async function editStock(req, res) {
+  const stockId = req.params.stockId;
+  const { price, quantity, transactionType, transactionDate } = req.body;
+  const editStockQuery = `
+    UPDATE stocks
+    SET price = ${price}, quantity = ${quantity}, transaction_type = '${transactionType}',
+      transaction_date = '${transactionDate}'
+    WHERE stock_id = ${stockId}`;
+
+  try {
+    await pool.query(editStockQuery);
+
+    res.status(200).json({ successMsg: 'Successfully edited the stock info' });
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ errorMsg: 'Internal Server Error' });
+  }
+}
+
+
+// @ROUTE         DELETE api/stock/:stockId
+// @DESCRIPTION   Delete Stock
+// @ACCESS        Private
+async function deleteStock(req, res) {
+  const stockId = req.params.stockId;
+
+  try {
+    await pool.query(`DELETE FROM stocks WHERE stock_id = ${stockId}`);
+
+    res.status(200).json({ successMsg: 'Successfully deleted the stock ' });
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ errorMsg: 'Internal Server Error' });
+  }
+}
+
+
 module.exports = {
-  addStock
+  addStock,
+  editStock,
+  deleteStock
 };
--- routes/stockRoute.js
+++ routes/stockRoute.js
@@ -4,12 +4,26 @@ const router = express.Router();
 const authMiddleware = require('../middlewares/authMiddleware');
 
 const {
-  addStock
+  addStock,
+  editStock,
+  deleteStock
 } = require('../controllers/stockControllers');
 
 // @ROUTE         POST api/stock
 // @DESCRIPTION   Add New Stock
 // @ACCESS        Private
 router.post('/', authMiddleware, addStock);
+
+
+// @ROUTE         PUT api/stock/:stockId
+// @DESCRIPTION   Edit Stock's Information
+// @ACCESS        Private
+router.put('/:stockId', authMiddleware, editStock);
+
+
+// @ROUTE         DELETE api/stock/:stockId
+// @DESCRIPTION   Delete Stock
+// @ACCESS        Private
+router.delete('/:stockId', authMiddleware, deleteStock);
 
 module.exports = router;
