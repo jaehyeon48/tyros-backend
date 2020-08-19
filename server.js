de083f9e9bd1bfd0ea71f3af98341f3e44406739
jaehyeon48 <imjaehyeon48@gmail.com>
2020-08-19 18:32:59 +0900

    Modified static avatars path

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors({
 }));
 app.use(express.json({ extended: false }));
 app.use(cookieParser());
-app.use('/avatars', express.static(path.join('avatars')));
+app.use('/avatars', express.static(path.join('/avatars')));
 
 app.use('/api/auth', require('./routes/authRoute'));
 app.use('/api/portfolio', require('./routes/portfolioRoute'));
