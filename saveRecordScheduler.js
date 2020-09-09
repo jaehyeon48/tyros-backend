4ffcafb3e8ce49f31b27990497fddee09298f3a7
jaehyeon48 <imjaehyeon48@gmail.com>
2020-09-09 20:57:25 +0900

    added checking market's status function

--- saveRecordScheduler.js
+++ saveRecordScheduler.js
@@ -1,13 +1,38 @@
+const axios = require('axios');
+const pool = require('./database/db');
 require('dotenv').config();
 
-function testfunc() {
-  console.log('hello world');
+async function saveRecordScheduler() {
+  if (checkMarketWasOpened()) {
+
+  }
 }
 
+
+// Check if the market 'was' opened for the day by comparing timestamp between current timestamp
+// and IEX api's timestamp and if the difference between the two is less than 35 minutes,
+// return true. The reason of 35 minutes is the schedule is running at 8:30PM UTC (which is 30 minutes
+// after the market's official closing time) plus 5 minutes of margin in case of a latency.
 function checkMarketWasOpened() {
-  // const apiUrl = `https://cloud.iexapis.com/stable/stock/aapl/quote?token=${process.env.IEX_CLOUD_API_KEY}`;
+  const apiUrl = `https://cloud.iexapis.com/stable/stock/aapl/quote?token=${process.env.IEX_CLOUD_API_KEY}`;
 
-  console.log(process.env.IEX_CLOUD_API_KEY);
+  try {
+    const marketStatusResponse = await axios.get(apiUrl);
+    const currentTimestamp = new Date().getTime();
+    const latestTimestamp = marketStatusResponse.data.lastTradeTime;
+
+    const minutesDifference = Math.floor((currentTimestamp - latestTimestamp) / 1000 / 60);
+    if (minutesDifference < 35) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
 }
 
-checkMarketWasOpened();
+
+saveRecordScheduler();
