c351261a05efb6f661f6de0871c8304b7fae4435
jaehyeon48 <imjaehyeon48@gmail.com>
2020-09-09 23:14:04 +0900

    added organizing stocks logic

--- saveRecordScheduler.js
+++ saveRecordScheduler.js
@@ -8,10 +8,12 @@ async function saveRecordScheduler() {
 
     for (const portfolioId of portfolios) {
       const userId = await getUserIdByPortfolioId(portfolioId);
+
+      const stocks = await getStockData(userId, portfolioId);
+      const organizedStocks = await sortStocks(stocks); // organize stocks by ticker
     }
   }
 }
-
 
 // Check if the market 'was' opened for the day by comparing timestamp between current timestamp
 // and IEX api's timestamp and if the difference between the two is less than 35 minutes,
@@ -42,7 +44,7 @@ async function getUserIdByPortfolioId(portfolioId) {
   try {
     const [userListRows] = await pool.query(`SELECT ownerId FROM portfolios WHERE portfolioId = ${portfolioId}`);
 
-    return userListRows[0];
+    return userListRows[0].ownerId;
   } catch (error) {
     console.error(error);
   }
@@ -75,10 +77,73 @@ async function getStockData(userId, portfolioId) {
         ON users.userId = stocks.holderId AND portfolios.portfolioId = stocks.portfolioId
       ORDER BY stocks.ticker, stocks.transactionDate, stocks.transactionType;`;
     const [stocksRow] = await pool.query(getStocksQuery);
-    console.log(stocksRow);
+    return stocksRow;
   } catch (error) {
     console.error(error);
   }
+}
+
+async function sortStocks(stocksList) {
+  let organizedShares = [];
+  const groupedStocks = groupStocksByTickerName(stocksList);
+  for (let [ticker, value] of Object.entries(groupedStocks)) {
+    organizedShares.push(await organizeGroupedStocks(ticker, value));
+  }
+  return organizedShares;
+}
+
+function groupStocksByTickerName(stocks) {
+  const stockGroup = {}
+  const tickers = [];
+  stocks.forEach(share => {
+    const tickerOfShare = share.ticker.toLowerCase();
+    const isTickerExist = tickers.findIndex(ticker => ticker === tickerOfShare);
+    if (isTickerExist === -1) tickers.push(tickerOfShare);
+  });
+
+  tickers.forEach(ticker => {
+    stockGroup[ticker] = [];
+  });
+
+  stocks.forEach(share => {
+    stockGroup[share.ticker.toLowerCase()].push(share);
+  });
+
+  return stockGroup;
+};
+
+async function organizeGroupedStocks(ticker, shareInfo) {
+  shareInfo.sort((a, b) => (a.transactionType < b.transactionType) ? 1 : ((b.transactionType < a.transactionType) ? -1 : 0));
+  const share = {};
+  let totalCost = 0;
+  let totalQty = 0;
+  share.ticker = ticker;
+
+  let sellQty = 0;
+  shareInfo.forEach(share => {
+    if (share.transactionType === 'sell') {
+      sellQty += share.quantity;
+    }
+    else if (share.transactionType === 'buy') {
+      const shareQty = share.quantity - sellQty;
+      if (shareQty > 0) {
+        totalCost += share.price * shareQty;
+        totalQty += shareQty;
+        sellQty = 0;
+      } else if (shareQty < 0) {
+        sellQty = -shareQty;
+      } else {
+        sellQty = 0;
+      }
+    }
+  });
+
+  share.avgCost = Number((totalQty <= 0 ? 0 : (totalCost / totalQty).toFixed(2)));
+  share.quantity = (totalQty <= 0 ? 0 : totalQty);
+
+  share.dailyReturn = null;
+  share.overallReturn = null;
+  return share;
 }
 
 saveRecordScheduler();
