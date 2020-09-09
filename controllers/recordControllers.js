47c4825455f3568b67b0fee5e2302ff56b9411db
jaehyeon48 <imjaehyeon48@gmail.com>
2020-09-09 13:39:55 +0900

    added get record route and its controller

--- controllers/recordControllers.js
+++ controllers/recordControllers.js
@@ -1,15 +1,33 @@
 const pool = require('../database/db');
 
+
+// @ROUTE         GET api/record
+// @DESCRIPTION   Get A Record
+// @ACCESS        Private
+async function getRecord(req, res) {
+  const userId = req.user.id;
+
+  try {
+    const [recordsRow] = await pool.query(`SELECT dailyReturn, totalValue, recordDate FROM dailyRecords WHERE userId = ${userId} ORDER BY recordDate asc`);
+    return res.status(200).json({ records: recordsRow });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ errorMsg: 'Internal Server Error' });
+  }
+}
+
 // @ROUTE         POST api/record
 // @DESCRIPTION   Add A New Record
 // @ACCESS        Private
 async function addNewRecord(req, res) {
   const userId = req.user.id;
-  const { dailyReturn, totalValue } = req.body;
+  const { dailyReturn, totalValue, recordDate } = req.body;
+
+  console.log(dailyReturn, totalValue, recordDate)
 
   try {
-    await pool.query(`INSERT INTO dailyRecords (userId, dailyReturn, totalValue)
-    VALUES (${userId}, ${dailyReturn}, ${totalValue})`);
+    await pool.query(`INSERT INTO dailyRecords (userId, dailyReturn, totalValue, recordDate)
+    VALUES (${userId}, ${dailyReturn}, ${totalValue}, '${recordDate}')`);
     return res.status(201).json({ successMsg: 'Daily record successfully added.' });
   } catch (error) {
     console.error(error);
@@ -18,5 +36,6 @@ async function addNewRecord(req, res) {
 }
 
 module.exports = {
+  getRecord,
   addNewRecord
 };
--- routes/recordRoute.js
+++ routes/recordRoute.js
@@ -4,8 +4,15 @@ const router = express.Router();
 const authMiddleware = require('../middlewares/authMiddleware');
 
 const {
+  getRecord,
   addNewRecord
 } = require('../controllers/recordControllers');
+
+
+// @ROUTE         GET api/record
+// @DESCRIPTION   Get A Record
+// @ACCESS        Private
+router.get('/', authMiddleware, getRecord);
 
 // @ROUTE         POST api/record
 // @DESCRIPTION   Add A New Record
